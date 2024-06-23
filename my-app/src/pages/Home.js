861740a162880b8965fa1d9fb51fa4c2e8f9d756
Angewoah <areoligio@gmail.com>
2024-06-23 15:07:00 -0400

    add picture in front page

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,6 +1,10 @@
+import React from "react";
+import "./Home.css";
+
 export default function Home() {
     return (
         <div className="home">
+            <div className="home-image"></div>
         <h1>Welcome to Potato Reviews</h1>
         <p>Here you can find reviews of all the best potatoes!</p>
         <p>Click on the "Reviews" link above to see the reviews.</p>
